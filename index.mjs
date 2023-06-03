86ae98212b580a8fb56ce46c6dc3af490a49964a
ANJONGGIL <qws458@naver.com>
2023-06-03 16:39:48 +0900

    s3연동 로직 추가

--- index.mjs
+++ index.mjs
@@ -1,36 +1,47 @@
 import AWS from 'aws-sdk';
 const s3 = new AWS.S3();
 
-export const handler = async(event) => {
+// AWS 구성
+AWS.config.update({
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRETACCESS_KEY,
+  region: process.env.REGION
+});
 
-    // 예제 사용
-    uploadObject('my-bucket', 'example.txt', './example.txt');
+export const handler = async (event) => {
 
-    
-    // TODO implement
-    
-};
+  console.log(event)
 
-const uploadObject = async (bucketName, key, filePath) => {
+  const base64Image = event.imagedata;
+  const key = event.resouce
+
+  // Base64 디코딩
+  const imageBuffer = Buffer.from(base64Image, 'base64');
+  console.log("디코딩 잘됬어요");
+
+  try {
+    // S3 업로드 설정
     const params = {
-      Bucket: bucketName,
-      Key: key,
-      Body: require('fs').createReadStream(filePath),
+      Bucket: 'modyeo-image',
+      Key: key, // 업로드 될 파일의 경로와 이름
+      Body: imageBuffer,
+      ContentType: 'image/jpeg' // 이미지 파일의 MIME 유형에 맞게 변경
     };
-  
-    try {
-      const result = await s3.upload(params).promise();
-      const response = {
-        statusCode: 200,
-        body: JSON.stringify(result),
+    console.log("파라미터 잘됬어요");
+
+    // S3로 파일 업로드
+    const uploadResult = await s3.upload(params).promise();
+    console.log('File uploaded successfully:', uploadResult.Location);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: uploadResult.Location })
     };
-    return response;
-    } catch (error) {
-        const response = {
-        statusCode: 500,
-        body: JSON.stringify('Error uploading object:', error),
+  } catch (err) {
+    console.error('Error reading or uploading file:', err);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Failed to upload file' })
     };
-    return response;
-      console.error('Error uploading object:', error);
-    }
-  };
+  }
+};
